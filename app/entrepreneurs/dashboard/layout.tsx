acd5c3bf657f7fb0a996111f7f693b33da8fc2e9
snamuelson26 <snamuelson@gmail.com>
2026-09-04 23:37:47 -0400

    Link Food Fans dashboard next action to communication system

--- app/entrepreneurs/dashboard/layout.tsx
+++ app/entrepreneurs/dashboard/layout.tsx
@@ -1,9 +1,15 @@
 import DashboardLocalization from "./DashboardLocalization";
+import EntrepreneurNextActionBridge from "./EntrepreneurNextActionBridge";
 
 export default function EntrepreneurDashboardLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <DashboardLocalization>{children}</DashboardLocalization>;
+  return (
+    <DashboardLocalization>
+      <EntrepreneurNextActionBridge />
+      {children}
+    </DashboardLocalization>
+  );
 }
